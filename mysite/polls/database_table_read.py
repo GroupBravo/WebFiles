505419c08a05940f46f0d86d226d29bdad26d288
GroupBravo <35933952+GroupBravo@users.noreply.github.com>
2018-04-10 18:18:43 +0100

    adding sql safe

--- mysite/polls/database_table_read.py
+++ mysite/polls/database_table_read.py
@@ -5,17 +5,23 @@
 
 # This module is used for accessing the database.
 #It will take the name of a given table that exists in the database and return all the data that is in that table
-
+from polls import sql_make_safe
 class load_data():
     #Upon initialisation it will create a connection to the database
     def __init__(self):
         import sqlite3
         self.db=sqlite3.connect("back_end_data")
         self.cursor=self.db.cursor()
-    # This function will collect data from the database because the database connection and return the data 
+    # This function will collect data from the database because the database connection and return the data
+
     def get_data(self,table):
-        data_to_run="SELECT * FROM "+table
-        self.cursor.execute(data_to_run)
-        user1 = self.cursor.fetchall()
-        self.db.close()
-        return(user1)
+        #A check is performed upon the inputted string data to make sure it is safe to be passed on to the database
+        if sql_make_safe.input_string(table)==True:
+            data_to_run="SELECT * FROM "+table
+            self.cursor.execute(data_to_run)
+            user1 = self.cursor.fetchall()
+            self.db.close()
+            return(user1)
+        else:
+            print("the data is not safe", table)
+            return("killlll")
