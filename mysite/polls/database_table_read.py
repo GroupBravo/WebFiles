dfc864818ea541243062cd381942e3b212f8863e
GroupBravo <35933952+GroupBravo@users.noreply.github.com>
2018-04-10 17:30:26 +0100

    add comments

--- mysite/polls/database_table_read.py
+++ mysite/polls/database_table_read.py
@@ -2,12 +2,17 @@
 
 #http://www.pythoncentral.io/introduction-to-sqlite-in-python/
 
-#database_table_read
+
+# This module is used for accessing the database.
+#It will take the name of a given table that exists in the database and return all the data that is in that table
+
 class load_data():
+    #Upon initialisation it will create a connection to the database
     def __init__(self):
         import sqlite3
         self.db=sqlite3.connect("back_end_data")
         self.cursor=self.db.cursor()
+    # This function will collect data from the database because the database connection and return the data 
     def get_data(self,table):
         data_to_run="SELECT * FROM "+table
         self.cursor.execute(data_to_run)
